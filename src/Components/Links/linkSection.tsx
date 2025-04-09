4f9d2195385c5a75e6b2f8895150a52b5f8ca2c5
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-04-09 16:09:23 -0300

    fix : corrige bugs

--- src/Components/Links/linkSection.tsx
+++ src/Components/Links/linkSection.tsx
@@ -37,7 +37,7 @@ export const LinkSection = () => {
       <Card href="https://fourcoders.com/" target="_blank" data-aos="fade-left" data-aos-duration="600">
         <CardImage src="images/10.png"/>
       </Card>
-      <Card  href="https://wa.me/+5511964046267?text=oii%20vimpelo%20seu%20agregador%20de%20links%20do%20instagram%20e%20quero%20saber%20masis" data-aos="fade-right" data-aos-duration="600">
+      <Card  href="https://wa.me/+5511964046267?text=oii%20vim%20pelo%20seu%20agregador%20de%20links%20do%20instagram%20e%20quero%20saber%20mais" data-aos="fade-right" data-aos-duration="600">
         <CardImage src="images/11.png" />
       </Card>
       <Card href="http://linkedin.com/in/luiz-antonio-7ab005215/" data-aos="fade-left" data-aos-duration="600">
